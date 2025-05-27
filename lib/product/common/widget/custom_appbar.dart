2a53c6f13ff719f84b29b1ffc505084476730f00
HakanOzsoyler <ozsoylerhakan@hotmail.com>
2025-05-27 15:02:38 +0300

    exit button created

--- lib/product/common/widget/custom_appbar.dart
+++ lib/product/common/widget/custom_appbar.dart
@@ -3,14 +3,21 @@ import 'package:flutter/material.dart';
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final PreferredSizeWidget? bottom;
+  final bool? isExit;
 
-  const CustomAppBar({super.key, required this.title, this.bottom});
+  const CustomAppBar({super.key, required this.title, this.bottom, this.isExit});
   @override
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(title),
       actions: [],
-      leading: IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app, color: Theme.of(context).primaryColor)),
+      leading:
+          isExit == null || isExit == false
+              ? SizedBox()
+              : IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.exit_to_app, color: Theme.of(context).primaryColor, size: 28),
+              ),
       bottom: bottom,
     );
   }
